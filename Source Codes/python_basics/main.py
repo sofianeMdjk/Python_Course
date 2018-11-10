316a4c22926674ec54e606a6ba58d150f287afb8
sofiane_mdjk <sofiane.medjkoune@protonmail.com>
2018-11-10 12:35:22 +0100

    Added the documentation and the solution for the first exercice

---
+++ Source Codes/python_basics/main.py
@@ -0,0 +1,9 @@
+print("I need you to gove an answer to the following operation : \n 100/20 - 30*0.5 + 12")
+
+answer = int(input())
+
+if answer == 2:
+    print("Congratulations, you rock at math")
+else :
+    for i in range(0,1000) :
+        print("I suck at math, i have to practice a little more")
